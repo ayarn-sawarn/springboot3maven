2e09bb0eba8dd0609657f80e699101e49c7d5378
ayarn-sawarn <ayarn.sawarn@oodles.io>
2023-03-24 00:01:17 +0530

    swagger url

--- src/main/java/com/mysqltutriol/MysqlDemoApplication.java
+++ src/main/java/com/mysqltutriol/MysqlDemoApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableSwagger2
 public class MysqlDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlDemoApplication.class, args);
 	}
 
+	//http://localhost:8080/swagger-ui/index.html
 }
